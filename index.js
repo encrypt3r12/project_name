fbc81bacf579add1516704a2a9f08ac40e16dc08
encrypt3r12 <46247930+encrypt3r12@users.noreply.github.com>
2022-06-16 18:54:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 console.log("Helo");
 
+console.log("Helo");
+
 app.get('/', (req, res) => res.send('Hello form Node.js Server'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
