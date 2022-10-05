4ec96cf19459cae1a7fc4b5f15e56226abefddf0
encrypt3r12 <46247930+encrypt3r12@users.noreply.github.com>
2022-10-05 17:33:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+const redis = require('redis');
+const client = redis.createClient({ url: process.env.DATABASE_URL });
+client.connect().then((res) => {
+  client.set('manav', 'test');
+});
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
