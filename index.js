6eb999d24855f0971f481e4790083c63310cceb8
Manav Agarwal <dpsman13016@gmail.com>
2022-10-06 05:26:44 +0530

    Minor

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+var pg = require('pg');
+const mongoose = require('mongoose');
 
 const redis = require('redis');
-const client = redis.createClient({ url: process.env.DATABASE_URL });
-client.connect().then((res) => {
-  client.set('manav', 'test');
-});
+
+try {
+  console.log('Redis-cli started');
+  const client = redis.createClient({ url: process.env.DATABASE_URL });
+  client
+    .connect()
+    .then((res) => {
+      console.log(res);
+      client.set('manav', 'test');
+      console.log('Redis-cli Successfully ended');
+    })
+    .catch((err) => {
+      console.log('11Redis-cli Error occured');
+      console.log(err);
+    });
+} catch (err) {
+  console.log('Redis-cli Error occured');
+  console.log(err);
+}
+
+try {
+  console.log('Postgres started');
+  var client = new pg.Client(process.env.DATABASE_URL);
+  client
+    .connect()
+    .then((res) => {
+      console.log(res);
+      console.log('Postgres Successfully ended');
+    })
+    .catch((err) => {
+      console.log('11Postgres Error occured');
+      console.log(err);
+    });
+} catch (err) {
+  console.log('Postgres Error occured');
+  console.log(err);
+}
+try {
+  console.log('Mongodb started');
+  mongoose
+    .connect(process.env.DATABASE_URL)
+    .then((res) => {
+      console.log(res);
+      console.log('MongoDB Successfully ended');
+    })
+    .catch((err) => {
+      console.log('11Mongodb Error occured');
+      console.log(err);
+    });
+} catch (err) {
+  console.log('Mongodb Error occured');
+  console.log(err);
+}
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node!\n';
   res.end(msg);
 });
 
