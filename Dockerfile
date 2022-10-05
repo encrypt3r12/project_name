69d22cd0bc721d2f10bdf8880788c7353331f9f6
Manav Agarwal <dpsman13016@gmail.com>
2022-10-05 16:45:39 +0530

    Minor

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
-from ubuntu
+FROM public.ecr.aws/m3t6x5f9/nodejs-16-alpine:latest
+EXPOSE 3000
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY package.json package.json
+RUN npm install
+COPY . .
+CMD ["npm","start"]
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-console.log("Helo");
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-console.log("He1lo");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);
+});
 
-console.log("HeA221DSFasdf2s2311lo");
-
-console.log("111Hesasdadssasdfwswswasdfs232wsw21sfs2aasdf11lo");
-
-console.log("Haeasdf2alo");
-
-app.get('/', (req, res) => res.send('Helasdflo form Node.js Server'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
